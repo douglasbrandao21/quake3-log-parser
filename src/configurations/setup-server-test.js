5da2cf9a15449edc29ed88df889b8089a1d949e9
douglasbrandao21 <douglasbrando5@gmail.com>
2022-04-14 11:34:31 -0300

    Correcting some jest configurations

--- src/configurations/setup-server-test.js
+++ src/configurations/setup-server-test.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const server = require("../server/server");
 const Game = require("../schemas/Game");
 
-var application;
-
 beforeAll(() => {
-  application = server.listen(process.env.PORT);
-
   mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
     user: process.env.MONGO_USER,
     pass: process.env.MONGO_PASS,
@@ -15,8 +11,6 @@ beforeAll(() => {
 });
 
 afterAll(async () => {
-  application.close();
-  
   mongoose.connection.close();
 });
 
